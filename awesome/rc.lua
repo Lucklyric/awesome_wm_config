0608068b87b6e3e9089ce27398b5e033c899584f
Alvin(Xinyao) Sun <xinyao1@ualberta.ca>
2019-03-14 11:21:28 -0600

    update

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -199,7 +199,7 @@ screen.connect_signal("property::geometry", set_wallpaper)
 awful.screen.connect_for_each_screen(function(s)
     -- Wallpaper
     -- set_wallpaper(s)
-    gears.wallpaper.maximized('/home/alvinsun/.config/awesome/bg/sd1Ky.jpg', s)
+    gears.wallpaper.maximized(os.getenv("HOME")..'/.config/awesome/bg/sd1Ky.jpg', s)
 
     -- Each screen has its own tag table.
     awful.tag({ "1", "2", "3", "4", "5", "6", "7", "8", "9" }, s, awful.layout.layouts[1])
